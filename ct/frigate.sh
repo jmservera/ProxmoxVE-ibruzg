1d6b1784afe781a178ec302401dfc37ccfd4bdfb
Bryzgalin <bruzgalin@gmail.com>
2025-08-18 13:54:40 +0300

    Change peros

--- ct/frigate.sh
+++ ct/frigate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/ibruzg/ProxmoxVE/refs/heads/frigate-0.16.0-test/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Authors: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -8,11 +8,12 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="Frigate"
 var_tags="${var_tags:-nvr}"
 var_cpu="${var_cpu:-4}"
-var_ram="${var_ram:-4096}"
+var_ram="${var_ram:-8192}"
 var_disk="${var_disk:-20}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
-var_unprivileged="${var_unprivileged:-0}"
+var_unprivileged="${var_unprivileged:-1}"
+var_verbose="${var_verbose:-yes}"
 
 header_info "$APP"
 variables
