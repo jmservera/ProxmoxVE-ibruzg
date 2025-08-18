2a5769cd9afc2693608e7f3cbf254d5d68d38e6b
Bryzgalin <bruzgalin@gmail.com>
2025-08-18 16:19:27 +0300

    Test libedgetpu1-max silent install

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -85,8 +85,12 @@ for i in $(seq 1 $TorTimeout); do
   $STD echo "Waiting Tor... ($i/$TorTimeout)"
   sleep 1
 done
-echo "libedgetpu1-max libedgetpu/accepted-eula boolean true" | debconf-set-selections
-echo "libedgetpu1-max libedgetpu/install-confirm-max boolean true" | debconf-set-selections
+if [[ "${VERBOSE}" == "no" ]]; then
+  sed -i '/^.*unset DEBIAN_FRONTEND.*$/d' /opt/frigate/docker/main/install_deps.sh
+  export DEBIAN_FRONTEND=noninteractive
+  echo "libedgetpu1-max libedgetpu/accepted-eula boolean true" | debconf-set-selections
+  echo "libedgetpu1-max libedgetpu/install-confirm-max boolean true" | debconf-set-selections
+fi
 $STD torify /opt/frigate/docker/main/install_deps.sh
 systemctl -q stop tor
 systemctl -q disable tor 
