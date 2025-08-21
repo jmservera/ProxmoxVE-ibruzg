814c4bdb999cfb234958609d13d2ad10160fb6ac
Bryzgalin <bruzgalin@gmail.com>
2025-08-21 13:16:01 +0300

    Test gpu selector

--- ct/frigate.sh
+++ ct/frigate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/ibruzg/ProxmoxVE/refs/heads/frigate-0.16.0-onx/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/ibruzg/ProxmoxVE/refs/heads/frigate-0.16.0-onx-sriov/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Authors: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
